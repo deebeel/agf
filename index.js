5899ede6507afe0cb3fb025f4337998cf0632eae
deebeel <deedeeua@gmail.com>
2015-08-13 00:07:23 +0300

    promise support, bug fixes

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 "use strict";
-const slice = [].slice;
 
-function run(generatorTemplate, done) {
-  const generator = generatorTemplate(resultHandler);
-  let isDone = false;
-  function resultHandler(isRaw) {
-    const bound = isRaw ? 0 : 1;
-    return (err) => {
-      if (err && !isRaw) {
-        const toCall = (done && done.bind(null, err)) || generator.throw.bind(generator, err);
-        return setImmediate(toCall);
-      }
-      const operationResult = slice.call(arguments, bound);
-      let toYield = null;
-      if(err && isRaw){
-        toYield = operationResult;
-      } else {
-        toYield = operationResult.length > 1 ? operationResult : operationResult[0];
-      }
-      setImmediate(() => {
-        if (isDone) {
-          operationResult.unshift(null);
-          return done.apply(null, operationResult);
+function buildResult(operationResult) {
+    return operationResult.length > 1 ? operationResult : operationResult[0];
+}
+
+function iterationResultHandler(resultHandler, done) {
+    return function (iterationResult) {
+        const value = iterationResult.value;
+        if (value instanceof Promise) {
+            const handler = resultHandler();
+            value
+                .then(handler.bind(null, null))
+                .catch(handler);
+        } else {
+            if (iterationResult.done && done) {
+                done.apply(null, [null, value]);
+            }
         }
-        const iterationResut = generator.next(toYield);
-        isDone = iterationResut.done;
-        if (isDone && done && typeof iterationResut.value !== "undefined") {
-          done(null, iterationResut.value);
-        }
-      });
     };
-  }
-  generator.next();
+}
+function run(generatorTemplate, done) {
+    const generator = generatorTemplate(resultHandler);
+    const iterationHandler = iterationResultHandler(resultHandler, done);
+
+    function resultHandler(isRaw) {
+        const bound = isRaw ? 0 : 1;
+        return function (err) {
+            if (err && !isRaw) {
+                return setImmediate(()=>(done && done(err)) || generator.throw(err));
+            }
+            const operationResult = Array.prototype.slice.call(arguments, bound);
+            const toYield = err && isRaw ? operationResult : buildResult(operationResult);
+            setImmediate(() => iterationHandler(generator.next(toYield)));
+        };
+    }
+
+   return iterationHandler(generator.next());
+
 }
 module.exports = run;
--- test/agf.test.js
+++ test/agf.test.js
@@ -1,89 +1,174 @@
 "use strict";
 const agf = require("../index.js");
 const domain = require("domain");
-describe("#agf", ()=>{
-  function asyncFunction(ret, fail){
-    return (cb) => {
-      if(fail){
-        return cb(ret);
-      }
+describe("#agf", ()=> {
+    function asyncFunction(ret, fail) {
+        return (cb) => {
+            if (fail) {
+                return cb(ret);
+            }
 
-      if(Array.isArray(ret)){
-        ret.unshift(null);
-        return cb.apply(null, ret);
-      }
-      cb(null, ret);
-    };
-  }
+            if (Array.isArray(ret)) {
+                ret.unshift(null);
+                return cb.apply(null, ret);
+            }
+            cb(null, ret);
+        };
+    }
 
-  it("should throw an error because of absence of the done callback", (done)=>{
-    const d = domain.create();
-    d.on("error", (e) => {
-      e.message.should.be.eql("error");
-      done();
+    it("should throw an error because of absence of the done callback", (done)=> {
+        const d = domain.create();
+        d.on("error", (e) => {
+            e.message.should.be.eql("error");
+            done();
+        });
+        d.run(() => {
+            agf(function* (resultHandler) {
+                const res = yield asyncFunction("res", false)(resultHandler());
+                res.should.be.eql("res");
+                yield asyncFunction(new Error("error"), true)(resultHandler());
+            });
+        });
+
     });
-    d.run(() => {
-      agf(function* (resultHandler){
-        const res = yield asyncFunction("res", false)(resultHandler());
-        res.should.be.eql("res");
-        return asyncFunction(new Error("error"), true)(resultHandler());
-      });
+
+
+    it("should return an error", (done)=> {
+        agf(function* (resultHandler) {
+            const e = yield asyncFunction(new Error("error"), true)(resultHandler({a: 100}));
+            e[0].should.be.an.Error();
+            e[0].message.should.be.eql("error");
+            done();
+        });
     });
 
-  });
-
-
-  it("should return an error", (done)=>{
-    agf(function* (resultHandler){
-      const e = yield asyncFunction(new Error("error"), true)(resultHandler(true));
-      e[0].should.be.an.Error();
-      e[0].message.should.be.eql("error");
-      done();
+    it("should return an array with results", (done)=> {
+        agf(function* (resultHandler) {
+            const res = yield asyncFunction([1, 2], false)(resultHandler(true));
+            res.should.be.an.Array();
+            res.should.be.eql([null, 1, 2]);
+            done();
+        });
     });
-  });
 
-  it("should return an array with results", (done)=>{
-    agf(function* (resultHandler){
-      const res = yield asyncFunction([1, 2], false)(resultHandler(true));
-      res.should.be.an.Array();
-      res.should.be.eql([null, 1, 2]);
-      done();
+    it("should invoke an async function gracefully", (done)=> {
+        agf(function* (resultHandler) {
+            const res = yield asyncFunction("res", false)(resultHandler());
+            res.should.be.String();
+            res.should.be.eql("res");
+            done();
+        });
     });
-  });
 
-  it("should invoke an async function gracefully", (done)=>{
-    agf(function* (resultHandler){
-      const res = yield asyncFunction("res", false)(resultHandler());
-      res.should.be.String();
-      res.should.be.eql("res");
-      done();
+    it("should successfully finalize the generator and return result to the callback", (done)=> {
+        agf(function* (resultHandler) {
+            const res = yield asyncFunction("resstart", false)(resultHandler());
+            res.should.be.String();
+            res.should.be.eql("resstart");
+            return yield asyncFunction("res", false)(resultHandler());
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql("res");
+            done();
+        });
     });
-  });
 
-  it("should successfully finalize the generator and return result to the callback", (done)=>{
-    agf(function* (resultHandler){
-      const res = yield asyncFunction("resstart", false)(resultHandler());
-      res.should.be.String();
-      res.should.be.eql("resstart");
-      return asyncFunction("res", false)(resultHandler());
-    }, (err, res) => {
-      (!err).should.be.true();
-      res.should.be.eql("res");
-      done();
+    it("should successfully finalize the generator and return result to the callback", (done)=> {
+        agf(function* (resultHandler) {
+            return yield asyncFunction("resstart", false)(resultHandler());
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql("resstart");
+            done();
+        });
     });
-  });
 
 
-  it("should successfully finalize the generator and return value passed to the return satement", (done)=>{
-    agf(function* (resultHandler){
-      const res = yield asyncFunction("resstart", false)(resultHandler());
-      res.should.be.String();
-      res.should.be.eql("resstart");
-      return 10;
-    }, (err, res) => {
-      (!err).should.be.true();
-      res.should.be.eql(10);
-      done();
+    it("should successfully finalize the generator and return just the result to the callback", (done)=> {
+        agf(function* () {
+            return 20;
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql(20);
+            done();
+        });
     });
-  });
+
+
+    it("should successfully resolve the promise, finalize the generator, and return just the result to the callback", (done)=> {
+        agf(function* () {
+            return yield Promise.resolve(20);
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql(20);
+            done();
+        });
+    });
+
+
+    it("should mix the promises with callbacked api and return just the result to the callback", (done)=> {
+        agf(function* (resultHandler) {
+            const p1 = Promise.resolve(2);
+            const p2 = Promise.resolve(3);
+            const p3 = yield Promise.resolve(20);
+            const c = yield asyncFunction(40, false)(resultHandler());
+            const pp = yield Promise.all([p1,p2]);
+            return pp.concat([p3,c]);
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql([2,3,20,40]);
+            done();
+        });
+    });
+
+    it("should reject the promise, finalize the generator, and return just the error to the callback", (done)=> {
+        agf(function* () {
+            return yield Promise.reject(new Error("error"));
+        }, (err) => {
+            err.should.be.an.Error();
+            err.message.should.be.eql("error");
+            done();
+        });
+    });
+
+    it("should successfully interrupt the generator and return result to the callback", (done)=> {
+        agf(function* (resultHandler) {
+            const res = yield asyncFunction(10, false)(resultHandler());
+            if (res === 10) {
+                return yield asyncFunction("res", false)(resultHandler());
+            }
+            return yield asyncFunction("unreached", false)(resultHandler());
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql("res");
+            done();
+        });
+    });
+
+
+    it("should successfully interrupt the generator and return a raw result to the callback", (done)=> {
+        agf(function* (resultHandler) {
+            const res = yield asyncFunction(10, false)(resultHandler());
+            if (res === 10) {
+                return yield asyncFunction(new Error("error"), true)(resultHandler());
+            }
+            return yield asyncFunction("unreached", false)(resultHandler());
+        }, (err) => {
+            err.should.be.an.Error();
+            done();
+        });
+    });
+
+    it("should successfully finalize the generator and return value passed to the return satement", (done)=> {
+        agf(function* (resultHandler) {
+            const res = yield asyncFunction("resstart", false)(resultHandler());
+            res.should.be.String();
+            res.should.be.eql("resstart");
+            return 10;
+        }, (err, res) => {
+            (!err).should.be.true();
+            res.should.be.eql(10);
+            done();
+        });
+    });
 });
